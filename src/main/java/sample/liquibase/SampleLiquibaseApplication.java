e9210fdfbb032f8220c02a06994bf4570b2c55c2
Joe Kutner <jpkutner@gmail.com>
2015-10-19 21:00:13 -0500

    Added controller and route

--- src/main/java/sample/liquibase/SampleLiquibaseApplication.java
+++ src/main/java/sample/liquibase/SampleLiquibaseApplication.java
@@ -18,12 +18,20 @@ package sample.liquibase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class SampleLiquibaseApplication {
+    @RequestMapping("/")
+        @ResponseBody
+        String home() {
+        return "Hello World!";
+    }
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SampleLiquibaseApplication.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleLiquibaseApplication.class, args);
+    }
 
 }
